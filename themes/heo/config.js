0cd63e1070e5a265ba9848c3d48f42d1b10344a7
OHKR <98163130+OHKR@users.noreply.github.com>
2024-02-19 06:54:48 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { 标题: '欢迎来到我的博客', url: 'https://notion-next-ten-jet-39.vercel.app/' },
-    { 标题: 'Welcome to BaiZang's Blog', url: 'https://notion-next-ten-jet-39.vercel.app/' }
+    { 标题: "Welcome to BaiZang's Blog", url: 'https://notion-next-ten-jet-39.vercel.app/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
